002ccc7af71405a720c40a79d9b7b41c6c599ff9
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-13 19:40:54 -0300

    first commit: project created

---
+++ src/test/java/com/tiagocordeiro/parkingapi/ParkingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagocordeiro.parkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
