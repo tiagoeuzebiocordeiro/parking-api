455abda3d1e83d144e79ded345642a94ce2e5d59
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-13 22:31:07 -0300

    same passwords throwable exception in update User password method

--- src/main/java/com/tiagocordeiro/parkingapi/service/UserService.java
+++ src/main/java/com/tiagocordeiro/parkingapi/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 @Service
 public class UserService {
 
@@ -25,6 +27,9 @@ public class UserService {
     @Transactional
     public User updatePassword(Long id, String password) {
         User user = findById(id);
+        if (Objects.equals(user.getPassword(), password)) {
+            throw new RuntimeException("The new password equals older password.");
+        }
         user.setPassword(password);
         return user;
     }
