efe7414e3b71231970d3c3fb7016d4dae55d6e77
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-13 21:36:08 -0300

    initial impl of create User method in UserService + create User endpoint in UserController

--- src/main/java/com/tiagocordeiro/parkingapi/service/UserService.java
+++ src/main/java/com/tiagocordeiro/parkingapi/service/UserService.java
@@ -1,8 +1,10 @@
 package com.tiagocordeiro.parkingapi.service;
 
+import com.tiagocordeiro.parkingapi.entity.User;
 import com.tiagocordeiro.parkingapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -10,4 +12,8 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Transactional
+    public User create(User obj) {
+        return repository.save(obj);
+    }
 }
--- src/main/java/com/tiagocordeiro/parkingapi/web/controller/UserController.java
+++ src/main/java/com/tiagocordeiro/parkingapi/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.tiagocordeiro.parkingapi.web.controller;
 
+import com.tiagocordeiro.parkingapi.entity.User;
 import com.tiagocordeiro.parkingapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,11 @@ public class UserController {
 
     @Autowired
     private UserService service;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User obj) {
+        User user = service.create(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
 }
