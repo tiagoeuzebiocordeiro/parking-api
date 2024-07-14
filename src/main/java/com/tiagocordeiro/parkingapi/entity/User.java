30eb23d0c84f7167de3f340012a77b2bb34c22cf
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-13 21:21:53 -0300

    User entity

---
+++ src/main/java/com/tiagocordeiro/parkingapi/entity/User.java
@@ -0,0 +1,66 @@
+package com.tiagocordeiro.parkingapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, unique = false, length = 200) // Length max will be 200 cause the password will get encrypted
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_date")
+    private String modifiedBy;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CUSTOMER
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
