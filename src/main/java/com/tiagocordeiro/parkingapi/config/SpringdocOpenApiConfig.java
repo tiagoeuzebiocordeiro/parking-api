1b9270273f499c7a6b9a9c91cd782ddaa0e721bd
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-16 21:17:09 -0300

    add Springdoc Open Api and Swagger 3 + SpringdocOpenApiConfig class

---
+++ src/main/java/com/tiagocordeiro/parkingapi/config/SpringdocOpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.tiagocordeiro.parkingapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringdocOpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(
+                new Info().title("REST API - Spring Parking")
+                        .description("API for car parking management")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact().name("Tiago Cordeiro").email("tiagoeuzebiocordeiro.ti@gmail.com"))
+        );
+    }
+}
