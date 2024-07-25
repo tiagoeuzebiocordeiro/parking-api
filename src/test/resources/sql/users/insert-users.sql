601fe55ff83eb5bc08938217ddf02fba37805add
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-24 21:33:53 -0300

    setup for test environment + sql files (insert and delete)

---
+++ src/test/resources/sql/users/insert-users.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_USERS (id, username, password, role) VALUES (100, 'test1@mail.com', '123456', 'ROLE_ADMIN');
+INSERT INTO TB_USERS (id, username, password, role) VALUES (101, 'test2@mail.com', '123456', 'ROLE_CUSTOMER');
+INSERT INTO TB_USERS (id, username, password, role) VALUES (102, 'test3@mail.com', '123456', 'ROLE_CUSTOMER');
