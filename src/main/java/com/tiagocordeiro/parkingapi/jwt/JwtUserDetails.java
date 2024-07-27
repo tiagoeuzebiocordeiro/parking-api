73eef6537b2b22c526059d4b9ee3bf9b9f0b0296
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-07-27 20:28:11 -0300

    JwtUserDetails class

---
+++ src/main/java/com/tiagocordeiro/parkingapi/jwt/JwtUserDetails.java
@@ -0,0 +1,25 @@
+package com.tiagocordeiro.parkingapi.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+
+    private com.tiagocordeiro.parkingapi.entity.User user;
+
+    public JwtUserDetails(com.tiagocordeiro.parkingapi.entity.User user) {
+        super(user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole().name()));
+        this.user = user;
+    }
+
+    public Long getId() {
+        return this.user.getId();
+    }
+
+    public String getRole() {
+        return this.user.getRole().name();
+    }
+}
