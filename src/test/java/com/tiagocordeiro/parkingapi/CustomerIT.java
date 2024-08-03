699838d962f118796cc7c809ae1c647307979bfb
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-03 12:44:12 -0300

    CREATE CUSTOMER TESTS for: cpf already registered in the system, using invalid data and using not authorized user role(role admin cannot create)

--- src/test/java/com/tiagocordeiro/parkingapi/CustomerIT.java
+++ src/test/java/com/tiagocordeiro/parkingapi/CustomerIT.java
@@ -2,6 +2,7 @@ package com.tiagocordeiro.parkingapi;
 
 import com.tiagocordeiro.parkingapi.web.dto.CustomerCreateDto;
 import com.tiagocordeiro.parkingapi.web.dto.CustomerResponseDto;
+import com.tiagocordeiro.parkingapi.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,6 +33,77 @@ public class CustomerIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getName()).isEqualTo("Tobias Marcondes");
         org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("65866701014");
+    }
+
+    @Test
+    public void createCustomer_WithCpfAlreadyRegisteredInTheSystem_ReturnsErrorMessage_WithStatus409() {
+        ErrorMessage responseBody = testClient.post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,
+                        "test4@mail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Tobias Marcondes", "19273528007"))
+                .exchange().expectStatus().isEqualTo(409).expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+
+    }
+    @Test
+    public void createCustomer_WithInvalidData_ReturnsErrorMessage_WithStatus422() {
+        ErrorMessage responseBody = testClient.post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,
+                        "test4@mail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("", ""))
+                .exchange().expectStatus().isEqualTo(422).expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient.post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,
+                        "test4@mail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Bob", "00000000000"))
+                .exchange().expectStatus().isEqualTo(422).expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient.post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,
+                        "test4@mail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Bob", "192.735.280-07"))
+                .exchange().expectStatus().isEqualTo(422).expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+    }
+
+    @Test
+    public void createCustomer_WithNotAuthorizedUserRole_ReturnsErrorMessage_WithStatus403() {
+        ErrorMessage responseBody = testClient.post()
+                .uri("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,
+                        "test1@mail.com", "123456"))
+                .bodyValue(new CustomerCreateDto("Tobias Marcondes", "19273528007"))
+                .exchange().expectStatus().isEqualTo(403).expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+
     }
 
 }
