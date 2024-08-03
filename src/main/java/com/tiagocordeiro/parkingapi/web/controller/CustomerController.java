5402cef95d0db587c7973d3bf6cd968f9d95cd13
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-03 12:12:11 -0300

    docs for customer create endpoint

--- src/main/java/com/tiagocordeiro/parkingapi/web/controller/CustomerController.java
+++ src/main/java/com/tiagocordeiro/parkingapi/web/controller/CustomerController.java
@@ -7,6 +7,11 @@ import com.tiagocordeiro.parkingapi.service.UserService;
 import com.tiagocordeiro.parkingapi.web.dto.CustomerCreateDto;
 import com.tiagocordeiro.parkingapi.web.dto.CustomerResponseDto;
 import com.tiagocordeiro.parkingapi.web.dto.mapper.CustomerMapper;
+import com.tiagocordeiro.parkingapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +31,18 @@ public class CustomerController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Create a new customer", description = "Resource for customer creation with user association. " +
+    "Requests needs a bearer token usage. Restrict access for CUSTOMER role.", responses = {
+            @ApiResponse(responseCode = "201", description = "Resource created successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "Customer's cpf already registered in the system.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "422", description = "Not processed resource because of less data information or " +
+                    "invalid data information.", content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "403", description = "Resource not authorized for ADMIN role", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+    })
     @PostMapping
     @PreAuthorize("hasRole('CUSTOMER')")
     public ResponseEntity<CustomerResponseDto> create(@RequestBody @Valid CustomerCreateDto createDto, @AuthenticationPrincipal JwtUserDetails userDetails) {
