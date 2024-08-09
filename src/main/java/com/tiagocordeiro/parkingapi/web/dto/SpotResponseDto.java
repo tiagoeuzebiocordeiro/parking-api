2384c5ff4c1604615d4933eefb9d795c4161a6f6
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-08 22:10:17 -0300

    quickfix, annotations at SpotResponseDto

--- src/main/java/com/tiagocordeiro/parkingapi/web/dto/SpotResponseDto.java
+++ src/main/java/com/tiagocordeiro/parkingapi/web/dto/SpotResponseDto.java
@@ -1,5 +1,11 @@
 package com.tiagocordeiro.parkingapi.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class SpotResponseDto {
     private Long id;
     private String code;
