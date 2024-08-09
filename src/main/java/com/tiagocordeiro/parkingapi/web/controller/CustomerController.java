ead42e2c8043a6665d0ddc7360149d21297227fa
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-08 22:11:25 -0300

    useless imports

--- src/main/java/com/tiagocordeiro/parkingapi/web/controller/CustomerController.java
+++ src/main/java/com/tiagocordeiro/parkingapi/web/controller/CustomerController.java
@@ -19,7 +19,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,8 +27,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 
