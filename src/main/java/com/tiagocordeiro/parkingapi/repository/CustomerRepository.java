e152585e149f9e82c9eae45a95a7a6ba645cda90
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2024-08-12 22:08:56 -0300

    CustomerRepository findByCpf method

--- src/main/java/com/tiagocordeiro/parkingapi/repository/CustomerRepository.java
+++ src/main/java/com/tiagocordeiro/parkingapi/repository/CustomerRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query(value = "SELECT * FROM TB_CUSTOMERS", nativeQuery = true)
     Page<CustomerProjection> findAllPageable(Pageable pageable);
 
     Customer findByUserId(Long id);
+
+    Optional<Customer> findByCpf(String cpf);
 }
